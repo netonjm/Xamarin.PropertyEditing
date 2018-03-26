a252861d33570553d40225b9cbb797b8de623b23
Eric Maupin <ermaup@microsoft.com>
2018-03-26 15:17:07 -0400

    [Core] Remove ilgen

--- Xamarin.PropertyEditing/Numeric.cs
+++ Xamarin.PropertyEditing/Numeric.cs
@@ -31,11 +31,9 @@ namespace Xamarin.PropertyEditing
 		private static Func<T, T> GetNullableVersion (Expression shiftExpression, Type realType)
 		{
 			var v = Expression.Parameter (typeof(T));
-			var convert = Expression.Call (Expression.Constant (NullConverter), nameof(NullableConverter.ConvertFrom), null, Expression.Convert (v, typeof(object)));
-			var shift = Expression.Invoke (shiftExpression, Expression.Convert (convert, realType));
+			var shift = Expression.Invoke (shiftExpression, Expression.Convert (Expression.Call (Expression.Constant (NullConverter), nameof(NullableConverter.ConvertFrom), null, Expression.Convert(v, typeof(object))), realType));
 			var conditional = Expression.Condition (Expression.Equal (v, Expression.Constant (null)), Expression.Default (realType), shift);
 
-			// (T v) => (T)((v == null) ? default(realType) : shiftExpression ((realType)NullableConverter.ConvertFrom ((object)v));
 			return Expression.Lambda<Func<T, T>> (Expression.Convert (conditional, typeof(T)), v).Compile();
 		}
 
@@ -52,14 +50,13 @@ namespace Xamarin.PropertyEditing
 			}
 
 			Task<Func<T,T>> createIncrement = Task.Run (() => {
+				var shiftby = Expression.Convert (Expression.Constant (1), t);
 				var value = Expression.Parameter (t);
 
 				Expression add;
 				if (t == typeof(byte) || t == typeof(sbyte)) {
-					// (t value) => (t)((int)value + 1);
 					add = Expression.Convert (Expression.Add (Expression.Convert (value, typeof(int)), Expression.Constant (1)), t);
 				} else {
-					var shiftby = Expression.Convert (Expression.Constant (1), t);
 					add = Expression.Add (value, shiftby);
 				}
 
